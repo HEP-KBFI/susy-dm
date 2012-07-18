390157876312a3b913ad03870a1520385a91b139
jpata <joosep.pata@gmail.com>
2012-07-18 15:35:31 +0300

    changed nmssm ewsb runner

--- random_sampling/nmssm_runner_ewsb.py
+++ random_sampling/nmssm_runner_ewsb.py
@@ -1,8 +1,12 @@
 import sys
 import imp
-random_sampling = imp.load_source("random_sampling", "/home/joosep/susy/random_sampling.py")
+#random_sampling = imp.load_source("random_sampling", "random_sampling.py")
+#from . import random_sampling
+import random_sampling
 
 import subprocess
+import os
+import shutil
 
 N = 100
 modelname = "NMSSM"
@@ -21,22 +25,70 @@ def prepare_input_file(parameters, of="par.dat"):
         f.write("%s %f\n" % (name, val))
     f.close()
 
+def save_output(d, output, ofdir="out"):
+    if not os.path.exists(ofdir):
+        os.makedirs(ofdir)
+    shutil.move("par.dat", "%s/par_%d.dat" % (ofdir, d))
+    shutil.move("spectr.dat", "%s/spectr_%d.dat" % (ofdir, d))
+    shutil.move("decay.dat", "%s/decay_%d.dat" % (ofdir, d))
+    of = open("%s/out_%d.dat" % (ofdir, d), "w")
+    for l in output:
+        of.write(l)
+    of.close()
+    return
+
 if __name__=="__main__":
 
-    conf = random_sampling.read_parameters("./sampling_NMSSM_EWSB.cfg")
+    fromFile = False
+    saveGood = True
+
+    if not fromFile:
+        conf = random_sampling.read_parameters("./sampling_NMSSM_EWSB.cfg")
+    else:
+        pointFile = open("good.txt")
 
     good_points = []
-    for i in range(100):
-        s = random_sampling.sample(conf)
-        prepare_input_file(s)
-        (out, retcode) = call("par.dat")
-        print "%d %d %s" % (i, retcode, out[-1][:-1])
-        if retcode==0:
-            good_points.append(s)
+
+    if not fromFile:
+        for i in range(1):
+            s = random_sampling.sample(conf)
+            prepare_input_file(s)
+            (out, retcode) = call("par.dat")
+            print "%d %d %s" % (i, retcode, out[-1][:-1])
+            if retcode==0:
+                save_output(i, out)
+                good_points.append(s)
+
+    if fromFile:
+        i = 0
+        for line in pointFile:
+            s = {}
+            coords = line.split(" ")
+            coords = filter(lambda x: "=" in x, coords)
+            print coords
+            for c in coords:
+                coord = c.split("=")
+                s[coord[0]] = float(coord[1])
+            prepare_input_file(s)
+
+            (out, retcode) = call("par.dat")
+            print "%d %d %s" % (i, retcode, out[-1][:-1])
+            if retcode==0:
+                save_output(i, out)
+                good_points.append(s)
+            i += 1
+
     if len(good_points)>0:
         keys = good_points[0].keys()
+        if saveGood:
+            goodFile = open("good.txt", "w")
         for p in good_points:
             s = ""
             for k in keys:
+                if saveGood:
+                    goodFile.write("%s=%f " % (k, p[k]))
                 s += "%s: %.2f " % (k, p[k])
+            if saveGood: goodFile.write("\n")
             print s
+        if saveGood:
+            goodFile.close()
