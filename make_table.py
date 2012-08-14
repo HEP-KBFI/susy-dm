7e29e3d6dd58493229a6bdb3615940be2f1d964c
Joosep Pata <joosep.pata@gmail.com>
2012-08-14 19:11:29 +0200

    block read

--- make_table.py
+++ make_table.py
@@ -13,40 +13,45 @@ NProb = 54
 for i in range(1,NProb):
     NMSSMPoint["PROB%d"%i] = tables.BoolCol()
 
+
+def process_line(point, line):
+    try:
+        line_data = map(float, line.split())
+    except ValueError, e:
+        print "bad line in %s" % fn
+        continue
+
+    if not len(line_data)==80:
+        continue
+
+    point["h1_mass"] = line_data[11]
+    point["chi1_mass"] = line_data[22]
+    point["omg"] = line_data[26]
+    prob = False
+    for i in range(1,NProb):
+        p = bool(line_data[26+i])
+        point["PROB%d"%i] = p
+        if p: prob=True
+    point["PROB"] = prob #Set to True if any of PROB1-53 is True
+    point.append()
+    return
+
 if __name__=="__main__":
     h5file = tables.openFile("nmssm1.h5", mode = "w", title = "SUSY parameter space points")
     group = h5file.createGroup("/", 'NMSSM1', 'NMSSM points')
-    filters = tables.Filters(complevel=5, complib='blosc', fletcher32=False)
-    table = h5file.createTable(group, 'parspace', NMSSMPoint, "parameter space")
+    filters = tables.Filters(complevel=9, complib='blosc', fletcher32=False)
+    table = h5file.createTable(group, 'parspace', NMSSMPoint, "parameter space", expectedrows=int(sys.argv[2]))
     point = table.row
     files = glob.glob(sys.argv[1])
     print files
     for fn in files:
         print "Processing file %s" % fn
         f = open(fn, "r")
-
-        for line in f:
-            try:
-                line_data = map(float, line.split())
-            except ValueError, e:
-                print "bad line in %s" % fn
-                continue
-
-            if not len(line_data)==80:
-                continue
-
-            point["h1_mass"] = line_data[11]
-            point["chi1_mass"] = line_data[22]
-            point["omg"] = line_data[26]
-            prob = False
-            for i in range(1,NProb):
-                p = bool(line_data[26+i])
-                point["PROB%d"%i] = p
-                if p: prob=True
-            point["PROB"] = prob #Set to True if any of PROB1-53 is True
-
-
-            point.append()
+        lines = True
+        while lines:
+            lines = f.readlines(sizehint=10000000)
+            for line in lines:
+                process_line(point, line)
         f.close()
     h5file.close()
     print "All done"
