d5074d282fbb5c240fecbcc0678e40caf0a3a45f
Joosep Pata <joosep.pata@gmail.com>
2012-08-14 16:02:19 +0200

    contour plotting

--- study_table.py
+++ study_table.py
@@ -3,21 +3,83 @@ import tables
 import matplotlib.pyplot as plt
 import matplotlib.cm
 import numpy
+import sys
+import scipy
+import scipy.stats
+import logging
 
 h5file = tables.openFile("/Users/joosep/Desktop/nmssm1.h5", mode = "r")
 t = h5file.root.NMSSM1.parspace
+logging.basicConfig(level=logging.DEBUG)
 
-l = t.nrows
-tempfile = tables.openFile("temp.h5", mode="w")
-chi1_arr = tempfile.createCArray(tempfile.root,'chi1',tables.Float32Atom(),shape=(l,1))
-h1_arr = tempfile.createCArray(tempfile.root,'h1',tables.Float32Atom(),shape=(l,1))
-for x in xrange(l):
-	chi1_arr[x,0] = t[x]["chi1_mass"]
-	h1_arr[x,0] = t[x]["h1_mass"]
-	tempfile.flush()
+def chi_h_points(selection, recreate=True):
+	logger = logging
+	logger.debug("Starting to get chi1/h1 points")
+	li = t.getWhereList(selection)
+	sel = t.readCoordinates(li)
+	l = len(li)
+
+	if recreate:
+		logger.debug("Recreating file")
+		tempfile = tables.openFile("temp.h5", mode="w")
+		filters = tables.Filters(complevel=9, complib='zlib', fletcher32=True)
+		chi1_h1_arr = tempfile.createCArray(tempfile.root,'chi1_h1',tables.Float32Atom(),shape=(l,2), filters=filters)
+		logger.debug("Putting chi1_mass to file")
+		chi1_h1_arr[:,0] = sel[:]["chi1_mass"]
+		logger.debug("Putting h1_mass to file")
+		chi1_h1_arr[:,1] = sel[:]["h1_mass"]
+		tempfile.flush()
+	else:
+		logger.debug("Opening existing file")
+		tempfile = tables.openFile("temp.h5", mode="r")
+		chi1_h1_arr = tempfile.root.chi1_h1
+	logger.debug("Got chi1/h1 points: %d" % len(chi1_h1_arr[:,0]))
+	return ((chi1_h1_arr[:,0], chi1_h1_arr[:,1]), tempfile)
 
 m_low = 123
 m_high = 129
+
+def eval_density(points_x, points_y):
+	logging.debug("Starting to evaluate density with KDE")
+	xlow, xhigh = 0,500
+	ylow, yhigh = 100,130
+	x,y = numpy.mgrid[xlow:xhigh:1, ylow:yhigh:1]
+
+	logging.debug("Preparing grid points for evaluation")
+	evalpoints = numpy.vstack([x.ravel(), y.ravel()])
+	logging.debug("Preparing data points for evaluation")
+	density_points = numpy.vstack([points_x.T, points_y.T])
+	density_points_for_calc = density_points[:,0:1000000]
+	logging.debug("Preparing KDE")
+	gkde = scipy.stats.kde.gaussian_kde(density_points_for_calc)
+	logging.debug("Evaluating KDE over grid on %d points"% len(evalpoints.T))
+	densities = gkde.evaluate(evalpoints)
+	densities = numpy.reshape(densities.T, x.shape)
+
+	densities = densities*10000.0+1.0
+	densities = numpy.log(densities)
+	logging.debug("Done evaluating density")
+	return (x,y,density_points_for_calc, densities)
+
+if __name__=="__main__":
+	logging.debug("Getting data points")
+	((chi1, h1), tempfile) = chi_h_points("h1_mass>0")
+	logging.debug("Calculating density")
+	(x,y, density_points_for_calc, densities) = eval_density(chi1, h1)
+	fig = plt.figure()
+	ax1 = fig.add_subplot(111)
+	xlow, xhigh = min(chi1)-10,max(chi1)+10
+	ylow, yhigh = 123,129
+	plt.xlim(xlow, xhigh)
+	plt.ylim(ylow, yhigh)
+	logging.debug("Plotting density contours")
+	ax1.contour(x,y, densities, 5)
+	logging.debug("Plotting scatterpoints")
+	ax1.scatter(density_points_for_calc[0], density_points_for_calc[1], c="k", alpha=0.1, s=10.0)
+	plt.ylabel("Higgs mass GeV/c**2")
+	plt.xlabel("chi0 mass GeV/c**2")
+	plt.suptitle("NMSSM parameter scan")
+	plt.show()
 
 def points(sel):
 	#A = [(x["h1_mass"], x["chi1_mass"]) for x in t.where(sel)]
