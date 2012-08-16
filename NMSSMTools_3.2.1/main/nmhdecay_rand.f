3766b54504c29463f298b8a6866ca2cdf388f71a
Joosep Pata <joosep.pata@gmail.com>
2012-08-16 13:20:45 +0200

    output

--- NMSSMTools_3.2.1/main/nmhdecay_rand.f
+++ NMSSMTools_3.2.1/main/nmhdecay_rand.f
@@ -344,6 +344,8 @@
        PAR(6)=AKMIN+(AKMAX-AKMIN)*RAN2(IDUM)
       ENDIF
 
+      WRITE(0,*)SQMQ3MIN
+      WRITE(0,*)SQMQ3MAX
 *     Choose random squark masses
       IF(SQMQ3MIN.EQ.SQMQ3MAX)THEN
        PAR(7)=SQMQ3MIN
