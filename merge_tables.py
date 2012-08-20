9ca2e06e96e811d355c1c7d46c5a3780ffba10b4
Joosep Pata <joosep.pata@gmail.com>
2012-08-20 16:46:24 +0200

    fixed merge

--- merge_tables.py
+++ merge_tables.py
@@ -5,15 +5,20 @@ import sys
 if __name__=="__main__":
 	of = tables.openFile("out.h5", "w")
 	infiles = map(tables.openFile, glob.glob(sys.argv[1]))
+	print "Copying first table"
 	infiles[0].root.parspace.copy(of.root)
 	ot = of.root.parspace
+	print "Output table has %d rows" % ot.nrows
 	colname = of.root.parspace.colnames[0]
 	dummySel = colname +"=="+colname
+
+	n = 1
 	for inf in infiles[1:]:
 		t = inf.root.parspace
-		print "Putting %d rows from %s" % (t.nrows, str(inf))
+		print "Putting %d rows from %s (%d/%d)" % (t.nrows, str(inf.filename), n, len(infiles))
 		t.whereAppend(ot, dummySel)
 		inf.close()
 		of.flush()
 		print "Output table has %d rows" % ot.nrows
+		n += 1
 	of.close()
