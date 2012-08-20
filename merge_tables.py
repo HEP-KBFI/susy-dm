d7cd9235683598be8a6f91b97c4deb787bba851b
Joosep Pata <joosep.pata@gmail.com>
2012-08-20 16:37:30 +0200

    fixed merge

--- merge_tables.py
+++ merge_tables.py
@@ -5,9 +5,9 @@ import sys
 if __name__=="__main__":
 	of = tables.openFile("out.h5", "w")
 	infiles = map(tables.openFile, glob.glob(sys.argv[1]))
-	infiles[0].parspace.copy(of.root)
-	for infn in infiles[1:]:
-		inf.NMSSM1.parspace.whereAppend(of.root.parspace, of.parspace.colnames[0] + "==" + of.parspace.colnames[0])
+	infiles[0].root.parspace.copy(of.root)
+	for inf in infiles[1:]:
+		inf.root.parspace.whereAppend(of.root.parspace, of.parspace.colnames[0] + "==" + of.parspace.colnames[0])
 		inf.close()
 		of.flush()
 	of.close()
