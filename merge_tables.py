9708329f263f4f4dc1059d4ecf95920873f74f18
Joosep Pata <joosep.pata@gmail.com>
2012-08-20 16:50:11 +0200

    added timing to merge_tables

--- merge_tables.py
+++ merge_tables.py
@@ -1,11 +1,12 @@
 import tables
 import glob
 import sys
+import time
 
 if __name__=="__main__":
 	of = tables.openFile("out.h5", "w")
 	infiles = map(tables.openFile, glob.glob(sys.argv[1]))
-	print "Copying first table"
+	print "Copying first table from file %s" % infiles[0].filename
 	infiles[0].root.parspace.copy(of.root)
 	ot = of.root.parspace
 	print "Output table has %d rows" % ot.nrows
@@ -14,6 +15,7 @@ if __name__=="__main__":
 
 	n = 1
 	for inf in infiles[1:]:
+		t0 = time.time()
 		t = inf.root.parspace
 		print "Putting %d rows from %s (%d/%d)" % (t.nrows, str(inf.filename), n, len(infiles))
 		t.whereAppend(ot, dummySel)
@@ -21,4 +23,6 @@ if __name__=="__main__":
 		of.flush()
 		print "Output table has %d rows" % ot.nrows
 		n += 1
+		t1 = time.time()
+		print "Copying took %d seconds" % (t1-t0)
 	of.close()
