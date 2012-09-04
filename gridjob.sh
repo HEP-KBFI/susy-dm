d192c0a5fc1b7ca0c958f2c5b53154e3ddb5ffa6
Joosep Pata <joosep.pata@gmail.com>
2012-09-04 16:09:24 +0200

    gridjob

--- gridjob.sh
+++ gridjob.sh
@@ -9,9 +9,9 @@ OUTDIR=/afs/cern.ch/user/j/jpata/work/public/
 cp -R $BASEDIR $NMSSMDIR
 cd $NMSSMDIR
 make
-cat randinp_NMSSM2.dat.template | sed "s/SEEDVAL/$RANDOM/" > randinp_$TAG.dat 
+cat inp.template | sed "s/SEEDVAL/$RANDOM/" > randinp_$TAG.dat 
 cd main
-./nmhdecay_rand < ../randinp_$TAG.dat | grep "[\d\s\+-E\.]+" | bzcat -z9 > ../randout_$TAG.dat.bz2
+./nmspec_rand < ../randinp_$TAG.dat | grep "[\d\s\+-E\.]+" | bzcat -z9 > ../randout_$TAG.dat.bz2
 cd ..
 rsync randout_$TAG.dat.bz2 $OUTDIR/
 rm -Rf $NMSSMDIR
